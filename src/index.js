54e9da7b9fe2d6666e794cb23c549ac2eafe0f44
mouarius <mouarius@gmail.com>
2021-01-05 20:20:31 +0100

    functionnalities and tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -8,19 +8,65 @@ const store = createStore(reducer)
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
+      type: 'GOOD',
     })
+  }
+  const neutral = () => {
+    store.dispatch({
+      type: 'OK',
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD',
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO',
+    })
+  }
+  const all = () => {
+    const state = store.getState()
+    return state.good + state.ok + state.bad
+  }
+  const average = () => {
+    const state = store.getState()
+    const total = all()
+    if (total !== 0) {
+      const average = (state.good - state.bad) / total
+      return average
+    }
+    return 0
+  }
+  const positive = () => {
+    const state = store.getState()
+    const total = all()
+    if (total !== 0) {
+      const positive = (state.good / total) * 100
+      return positive
+    }
+    return 0
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div className="give-feedback">
+        <h2>give feedback</h2>
+        <button onClick={good}>good</button>
+        <button onClick={neutral}>neutral</button>
+        <button onClick={bad}>bad</button>
+        <button onClick={zero}>reset stats</button>
+      </div>
+      <div className="statistics">
+        <h2>statistics</h2>
+        <div>good : {store.getState().good}</div>
+        <div>neutral : {store.getState().ok}</div>
+        <div>bad : {store.getState().bad}</div>
+        <div>all : {all()}</div>
+        <div>average : {average()}</div>
+        <div>positive : {positive()} %</div>
+      </div>
     </div>
   )
 }
